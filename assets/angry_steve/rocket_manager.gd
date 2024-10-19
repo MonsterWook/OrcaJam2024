ac077e9dc4a315019806b4a67f2a6bb3e0200ef8
gnorris55 <graemenorris99@gmail.com>
2024-10-19 15:41:13 -0700

    added fuel progress bar

--- assets/angry_steve/rocket_manager.gd
+++ assets/angry_steve/rocket_manager.gd
@@ -2,18 +2,54 @@ extends Node2D
 
 const bullet = preload("res://assets/angry_steve/bullet.tscn")
 @onready var rocket_movement: Node2D = $RocketMovement
+@onready var shoot_timeout: Timer = $shootTimeout
+@onready var fuel_bar: ProgressBar = $RocketMovement/FuelBar
+
+var can_shoot = true
+var reload_time = 1.0
+var magazine_size = 5
+var fuel_amount = 100
+var toughness_percent = 1
+var magazine_amount = magazine_size
+
+var left_barrier = -100
+var right_barrier = 100
 
 func _init():
-	print("succesfully started")
+	pass
+
+func _ready():
+	fuel_bar.max_value = fuel_amount
 
 
 func _process(delta: float) -> void:
 	# if the shoot butten is pressed spawn bullet at the gun spawn point with the
 	# movement direction of the rotation of the rocket
-	if Input.is_action_just_pressed("boost"):
-		var result = rocket_movement.get_bullet_stats()	
-		var instance = bullet.instantiate()
-		instance.position = result[0]
-		instance.direction_vec = result[1]
-		add_child(instance)
+	if Input.is_action_just_pressed("boost") and can_shoot:
 		
+		if (magazine_amount < 1):
+			magazine_amount = magazine_size
+			can_shoot = false
+			shoot_timeout.start()
+		else:
+			var result = rocket_movement.get_bullet_stats()	
+			var instance = bullet.instantiate()
+			instance.position = result[0]
+			instance.direction_vec = result[1]
+			add_child(instance)
+			magazine_amount -= 1
+			
+	if Input.is_action_just_pressed("bounce"):
+		#print("bounce")
+		take_damage(20)
+		
+		
+func take_damage(damage):
+	fuel_amount -= damage*toughness_percent
+	rocket_movement.bounce_off(10000)
+	fuel_bar.set_value(fuel_amount)
+	print(fuel_amount)
+
+func _on_timer_timeout() -> void:
+	print("can shoot again")
+	can_shoot = true
--- assets/angry_steve/rocket_movement.gd
+++ assets/angry_steve/rocket_movement.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
-const ROTATION_SPEED: float = 5
-const BOOST_SPEED: float = 300
+
 @onready var rigid_body_2d: RigidBody2D = $Rocket/RigidBody2D
 @onready var angry_steve: Node2D = $Rocket/RigidBody2D/AngrySteve
-@onready var rocket: Node2D = $Rocket
+#@onready var rocket: Node2D = $Rocket
 @onready var bullet_spawn: Node2D = $Rocket/RigidBody2D/BulletSpawn
+#@onready var timer: Timer = $Rocket/Timer
 
+
+const boost_speed: float = 500
+
+var rotation_speed: float = 200 
 var vertical_speed = -5
 var vertical_position = 0
 
 var max_rotation = deg_to_rad(60)
 var min_rotation = deg_to_rad(-60)
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,10 +24,10 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("move_left") and !(rigid_body_2d.rotation < min_rotation):
-		rigid_body_2d.apply_torque_impulse(-100)
+		rigid_body_2d.apply_torque_impulse(-rotation_speed)
 		
 	if Input.is_action_pressed("move_right") and !(rigid_body_2d.rotation > max_rotation):
-		rigid_body_2d.apply_torque_impulse(100)
+		rigid_body_2d.apply_torque_impulse(rotation_speed)
 			
 	if rigid_body_2d.angular_velocity < 0 and rigid_body_2d.rotation < min_rotation:
 		rigid_body_2d.angular_velocity = 0
@@ -33,18 +36,30 @@ func _physics_process(delta: float) -> void:
 	#print(rigid_body_2d.angular_velocity)
 		
 	var direction_vec = Vector2(sin(rigid_body_2d.rotation), -cos(rigid_body_2d.rotation))
-	rigid_body_2d.apply_force(direction_vec*BOOST_SPEED)
+	rigid_body_2d.apply_force(direction_vec*boost_speed)
 	vertical_position += delta*vertical_speed
 	
 func get_bullet_stats() -> Array:
 	rigid_body_2d.position.y = vertical_position
+	
 	var spawn_point = bullet_spawn.global_position
 	var direction_vec = Vector2(sin(rigid_body_2d.rotation), -cos(rigid_body_2d.rotation))
 	
-	#print(direction_vec)
-	
 	return [spawn_point, direction_vec]
 	
+func bounce_off(bounce_amount: int):
+	var bounce_direction = Vector2(-rigid_body_2d.linear_velocity.x, 0).normalized()
+	rigid_body_2d.apply_force(bounce_direction*bounce_amount)
+
 func _process(delta):
 	rigid_body_2d.position.y = vertical_position
+	#print(rigid_body_2d.position)
+	if (rigid_body_2d.position.x > 550 or rigid_body_2d.position.x < -550):
+		bounce_off(30000)
+		
+	#print(rigid_body_2d.linear_velocity)
 	
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
