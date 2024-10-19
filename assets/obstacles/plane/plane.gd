90235c959fe6f241686c58ea291788e4f8fa85bc
Wook <martinaranogabelsberger@gmail.com>
2024-10-18 23:00:51 -0700

    Removed scripts folder and created obstacles folder
    also started on the easy movement of some of the obstacles

---
+++ assets/obstacles/plane/plane.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var SPEED : int = 100
+@export var sin_wave_intensity : int = 25
+@export var sin_wave_speed : float = 2
+
+var spawnedLocation : bool = false #left = false, right = true
+var time : float
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time += delta
+	movement()
+
+func movement():
+	#easy movement just go from the left of the screen to the right or vice versa
+	if (spawnedLocation):
+		#move towards the left with the tiniest bit of sin movement
+		velocity = Vector2(-1 * SPEED, get_sine())
+	else:
+		#move towards the right with the tiniest bit of sin movement
+		velocity = Vector2(1 * SPEED, get_sine())
+	move_and_slide()
+	
+func get_sine():
+	return sin(time * sin_wave_speed) * sin_wave_intensity
