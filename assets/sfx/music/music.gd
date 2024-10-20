e0ae7d5bc44cf5add8ffcaec2664d5f19e123312
Wook <martinaranogabelsberger@gmail.com>
2024-10-20 16:58:08 -0700

    music final

--- assets/sfx/music/music.gd
+++ assets/sfx/music/music.gd
@@ -7,12 +7,19 @@ extends Node2D
 @onready var menu_music : AudioStreamPlayer = $MenuMusic
 
 func _process(delta):
-	if(gm.steve_state == gm.PLAYING_STATE):
+	if(gm.qte_playing):
+		menu_music.stop()
+		layer_1.stop()
+		layer_2.stop()
+	elif(gm.steve_state == gm.PLAYING_STATE):
 		if(!layer_1.playing):
 			layer_1.play()
+			layer_2.play()
 			menu_music.stop()
 
 	elif(gm.steve_state == gm.MENU_STATE):
 		if(!menu_music.playing):
 			menu_music.play()
 			layer_1.stop()
+			layer_2.stop()
+	
