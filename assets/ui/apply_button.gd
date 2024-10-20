131654b3a5ec5685de37b1b9f64ecd1dd52b68c2
caramxl <101217293+caramxl@users.noreply.github.com>
2024-10-20 16:26:22 -0700

    Shop sound effects added

--- assets/ui/apply_button.gd
+++ assets/ui/apply_button.gd
@@ -1,20 +1,15 @@
-extends CheckButton
+extends Button
 @onready var menu_click: AudioStreamPlayer = $MenuClick
 
-@export var index: int
-@export var cost: int
-var cost_text: String
-signal on_upgrade_pressed(button_index)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "%6s" % ""
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _pressed() -> void:
-	on_upgrade_pressed.emit(index)	
 	menu_click.play()
