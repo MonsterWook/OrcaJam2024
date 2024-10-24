a3915824970b40af70214f6713cb0c87a6591e05
Wook <martinaranogabelsberger@gmail.com>
2024-10-24 13:17:17 -0700

    improved qte

--- scenes/qte.gd
+++ scenes/qte.gd
@@ -7,6 +7,7 @@ extends Node2D
 @onready var anim = $AnimationPlayer
 @onready var space_bar = $space_bar
 @onready var progress_bar = $TextureProgressBar
+@onready var click = $space_bar/CLICK
 
 @onready var explosion = $sfx/Explosion
 @onready var scream = $sfx/Scream
@@ -28,14 +29,15 @@ func start_qte():
 	scream.play()
 	qte_active = true
 	space_bar.visible = true
+	space_bar.play()
 	progress_bar.visible = true
 	cutscene.play("qte")
-	anim.play("qte")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	progress_bar.set_value(duration)
+	click.set_value(mash_button)
 	if (mash_button >= amount_needed):
 		qte_win()
 	elif (qte_active && duration > 0):
@@ -46,6 +48,7 @@ func _process(delta):
 		qte_fail()
 func qte_win():
 	mash_button = 0
+	click.value = 0
 	qte_active = false
 	space_bar.visible = false
 	progress_bar.visible = false
@@ -53,6 +56,7 @@ func qte_win():
 
 func qte_fail():
 	mash_button = 0
+	click.value = 0
 	qte_active = false
 	space_bar.visible = false
 	progress_bar.visible = false
